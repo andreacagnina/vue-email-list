558ef4add6fbd486554dea0e9aba91a0835e9321
andreacagnina <amirkal@libero.it>
2024-07-25 15:09:56 +0200

    [upd]scaffolding

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp({
+    data() {
+        return {
+
+        }
+    },
+    created() {
+
+    },
+    watch: {
+
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+})
+
+app.mount('#app')
