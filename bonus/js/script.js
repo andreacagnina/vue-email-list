b2453c82e6a7834ff3b9bdd14871d7075b00c9fa
andreacagnina <amirkal@libero.it>
2024-07-25 16:55:28 +0200

    [fix] fix + add spinner

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -15,19 +15,20 @@ const app = Vue.createApp({
     computed: {
 
     },
-    // tramite axios chiedo la mail dall'API e DOPO attendo risposta. Se la lunghezza dell'array è inferiore a 10 allora verranno pushati i risultati nel mio array, altrimenti non accadrà nulla
+    // tramite axios chiedo la mail dall'API e DOPO attendo risposta. 
     methods: {
         generateRandomEmail() {
-            for (let i = 0; i <= 10; i++) {
+            this.cleanArray();
+            for (let i = 0; i < 10; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-                    (this.arrEmail.length < 10) ?
-                        (this.arrEmail.push(result.data.response)) : '';
+                    this.arrEmail.push(result.data.response)
                 });
             }
         },
         // quando riclicco sul bottone, viene avviata la funzione per ripulire i risultati precedenti
         cleanArray() {
             this.arrEmail = []
+
         }
 
     }
